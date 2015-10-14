20e2f2e0d2b7cacede3599cb1af1778cb7267230
baronenicola <baronenicola@gmail.com>
2015-10-14 02:53:11 +0200

    no message

--- full-gallery-magic/js/boxesFx.js
+++ full-gallery-magic/js/boxesFx.js
@@ -157,10 +157,10 @@
 		var self = this, currentPanel = this.panels[ this.current ];
 
 		if( dir === 'next' ) {
-			this.current = this.current < this.panelsCount - 1 ? this.current + 1 : 0;			
+			this.current = this.current < 7 - 1 ? this.current + 1 : 0;			
 		}
 		else {
-			this.current = this.current > 0 ? this.current - 1 : this.panelsCount - 1;
+			this.current = this.current > 0 ? this.current - 1 : 7 - 1;
 		}
 
 		// next panel to be shown
